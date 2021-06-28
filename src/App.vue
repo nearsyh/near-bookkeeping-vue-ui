7f03c180b67505f4059867563148861f13431f62
Near <nearsy.h@gmail.com>
2021-06-28 20:02:40 +0800

    Add Transactions component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Transactions />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
-import HelloWorld from './components/HelloWorld.vue'
+import Transactions from './components/Transactions.vue'
 
 @Options({
   components: {
-    HelloWorld
+    Transactions
   }
 })
 export default class App extends Vue {}
