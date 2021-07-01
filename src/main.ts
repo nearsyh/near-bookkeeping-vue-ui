a6ab697af7029b26c746c0b4944bd74a6a5d5af9
Near <nearsy.h@gmail.com>
2021-07-01 20:11:13 +0800

    Init pwa

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import { VueCookieNext } from 'vue-cookie-next';
 import App from './App.vue';
+import './registerServiceWorker';
 
 createApp(App).use(VueCookieNext).mount('#app');
 VueCookieNext.config({ expire: '30d' });
