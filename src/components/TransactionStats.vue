1e6707a66172d3632284278db2ed57e26176fc9d
Nearsyh <nearsyh@gmail.com>
2021-07-01 10:22:15 +0800

    Tweak stats rendering

--- src/components/TransactionStats.vue
+++ src/components/TransactionStats.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="transaction-stats">
-    <n-card :title="title">
-      <div>
-        <div>总收入: {{ totalIncome }}元</div>
-        <div>总支出: {{ totalExpense }}元</div>
+    <div class="header">
+      <span class="month">{{ month }}</span>
+      <span>月</span>
+    </div>
+    <div class="body">
+      <div class="digits-container">
+        <div>
+          支出 <span :class="expenseClass">￥{{ totalExpense }}</span>
+        </div>
+        <div>
+          收入 <span class="digits">￥{{ totalIncome }}</span>
+        </div>
       </div>
-    </n-card>
+    </div>
   </div>
 </template>
 
@@ -20,8 +28,18 @@ import { globalState } from '@/App.vue';
   }
 })
 export default class TransactionStats extends Vue {
-  get title() {
-    return `${globalState.transactions.month()}月`;
+  get month() {
+    return globalState.transactions.month();
+  }
+
+  get expenseClass() {
+    return globalState.transactions.totalExpense().cents > 10000 * 100
+      ? 'digits warning'
+      : 'digits';
+  }
+
+  get budgetRatio() {
+    return globalState.transactions.totalExpense().cents / 10000 / 100;
   }
 
   get totalIncome() {
@@ -34,4 +52,35 @@ export default class TransactionStats extends Vue {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.transaction-stats {
+  padding: 10px 40px 20px 40px;
+  background-color: #f1f1f1;
+  box-shadow: 0px 2px 2px 1px #e2e2e2;
+}
+
+.header {
+  font-size: large;
+  display: flex;
+  align-items: baseline;
+  margin-bottom: 5px;
+}
+
+.month {
+  font-size: 40px;
+  padding-right: 5px;
+}
+
+.digits-container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.digits {
+  font-weight: bold;
+}
+
+.warning {
+  color: red;
+}
+</style>
